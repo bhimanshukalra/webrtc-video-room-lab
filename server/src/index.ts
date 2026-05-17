599a55d9289f54ae91c6d850a847e1d2a5ec19a6
bhimanshukalra <bhimanshukalra@gmail.com>
2026-05-17 09:11:59 +0530

    Add Socket.IO room signaling server

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,11 +4,33 @@ import bodyParser from "body-parser";
 
 const PORT = 8000;
 
-const socketIo = new Server();
+const socketIo = new Server({
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
 const app = express();
 
 app.use(bodyParser.json());
 
-socketIo.on("connection", (socket) => console.log("connected", socket));
+const emailToSocketMapping = new Map();
+
+interface JoinRoomPayload {
+  emailId: string;
+  roomId: string;
+}
+
+socketIo.on("connection", (socket) => {
+  console.log("New connection");
+  socket.on("join-room", ({ emailId, roomId }: JoinRoomPayload) => {
+    emailToSocketMapping.set(emailId, socket.id);
+    socket.join(roomId);
+    socket.emit("joined-room", { roomId });
+    socket.broadcast.to(roomId).emit("user-joined", { emailId });
+    console.log("room joined", emailId, roomId);
+  });
+});
 
 app.listen(PORT, () => console.log(`Node server running at ${PORT}`));
+socketIo.listen(8001);
